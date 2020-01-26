c6ea09877c5edf15cc5055f0aa783b501f7fb70e
anthony gawbill <gaw316@gmail.com>
2020-01-25 19:03:36 -0500

    hello

---
+++ build/precache-manifest.37521e84bbc984cb390f72fbbb0c24f4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e29b224b070c3afa5cc0a4f5e31861",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49a8468d6983299e8d4e",
+    "url": "/static/css/10.23aeb280.chunk.css"
+  },
+  {
+    "revision": "5d22035aa5fe98d1550d",
+    "url": "/static/css/3.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b998832aa7608e0848e3",
+    "url": "/static/css/4.6b4e1072.chunk.css"
+  },
+  {
+    "revision": "11af9439954a62e1044a",
+    "url": "/static/css/5.4aba7add.chunk.css"
+  },
+  {
+    "revision": "0dc59cd6aacb24aab536",
+    "url": "/static/css/6.6f085965.chunk.css"
+  },
+  {
+    "revision": "b7fabd8ef26abf242db8",
+    "url": "/static/css/7.7f02b34e.chunk.css"
+  },
+  {
+    "revision": "1c340ba570df740619e3",
+    "url": "/static/css/8.b9cda2d8.chunk.css"
+  },
+  {
+    "revision": "9166f7b89c638ad06e25",
+    "url": "/static/css/9.0a9148d0.chunk.css"
+  },
+  {
+    "revision": "69c2c5071fac8ed453dd",
+    "url": "/static/css/main.70837814.chunk.css"
+  },
+  {
+    "revision": "b594ff2d32a4761e23f8",
+    "url": "/static/js/0.0ed38583.chunk.js"
+  },
+  {
+    "revision": "49a8468d6983299e8d4e",
+    "url": "/static/js/10.686c7b61.chunk.js"
+  },
+  {
+    "revision": "5d22035aa5fe98d1550d",
+    "url": "/static/js/3.b3a1769f.chunk.js"
+  },
+  {
+    "revision": "b998832aa7608e0848e3",
+    "url": "/static/js/4.791a0d36.chunk.js"
+  },
+  {
+    "revision": "11af9439954a62e1044a",
+    "url": "/static/js/5.a31ad04c.chunk.js"
+  },
+  {
+    "revision": "0dc59cd6aacb24aab536",
+    "url": "/static/js/6.ce2efc57.chunk.js"
+  },
+  {
+    "revision": "b7fabd8ef26abf242db8",
+    "url": "/static/js/7.4dd3e845.chunk.js"
+  },
+  {
+    "revision": "1c340ba570df740619e3",
+    "url": "/static/js/8.57e50321.chunk.js"
+  },
+  {
+    "revision": "9166f7b89c638ad06e25",
+    "url": "/static/js/9.a8f9f046.chunk.js"
+  },
+  {
+    "revision": "69c2c5071fac8ed453dd",
+    "url": "/static/js/main.ae77ab07.chunk.js"
+  },
+  {
+    "revision": "2f2b03b5b65a632d61e8",
+    "url": "/static/js/runtime~main.f831b21c.js"
+  },
+  {
+    "revision": "2c63847ac8deb09cdc279aa3d094a880",
+    "url": "/static/media/banner2.2c63847a.jpeg"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import keys from "./config/keys";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(
+  express.json({
+    type: ["application/json", "text/plain"]
+  })
+);
+
+app.post("/email", (req, res) => {
+  const nodemailer = require("nodemailer");
+  const { google } = require("googleapis");
+  const OAuth2 = google.auth.OAuth2;
+  const { clientId, clientSecret, redirectUrl, refreshToken } = keys();
+  const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+
+  const [name, email, subject, category, message] = [
+    req.body.name,
+    req.body.email,
+    req.body.subject,
+    req.body.category,
+    req.body.message
+  ];
+
+  oauth2Client.setCredentials({
+    refresh_token: refreshToken
+  });
+
+  const accessToken = oauth2Client.getAccessToken();
+
+  const smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "anthgawbill@gmail.com",
+      clientId,
+      clientSecret,
+      refreshToken,
+      accessToken
+    }
+  });
+
+  const mailOptions = {
+    from: "anthgawbill@gmail.com",
+    to: "anthonyga@pcom.edu",
+    subject: subject,
+    text: message,
+    html: `<b>Message from:</b> ${name}<br></br> <b>Email:</b> ${email} <br></br> 
+    <b>Regarding:</b> ${category}<br></br> <b>Message:</b> ${message}`
+  };
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response);
+      smtpTransport.close();
+      return res.send("success");
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
