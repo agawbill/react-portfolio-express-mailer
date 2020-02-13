b817455ea556ebabc24ad2a2f913a021aa1ef15f
anthony gawbill <gaw316@gmail.com>
2020-02-13 01:02:13 -0500

    adding server

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-export const prod = {
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  redirectUrl: "https://developers.google.com/oauthplayground",
-  refreshToken: process.env.REFRESH_TOKEN
-};
